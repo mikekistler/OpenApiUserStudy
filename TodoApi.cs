e146e3256729420a5c42383f9bf22febd1ee5860
Mike Kistler (from Dev Box) <mikekistler@microsoft.com>
2024-05-21 10:47:58 -0700

    Task 2 - Add features in OpenAPI document

--- TodoApi.cs
+++ TodoApi.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
 
 internal static class TodosApi
 {
@@ -13,32 +14,40 @@ internal static class TodosApi
     {
         var group = routes.MapGroup("/todos");
 
+        group.WithTags("Todos");
+
         // List all todos
-        group.MapGet("/", Ok<Todo[]> (int? offset, int? limit) =>
+        group.MapGet("/", Ok<Todo[]> ([Description("Offset of first item")] int? offset, [Description("Maximum number of items to return")] int? limit) =>
         {
             return TypedResults.Ok(todos.Values.Skip(offset ?? 0).Take(limit ?? int.MaxValue).ToArray());
-        });
+        })
+        .WithDescription("List all todos")
+        .WithName("ListTodos");
 
         // Get a specific todo by id
-        group.MapGet("/{id}", Results<Ok<Todo>, NotFound<ProblemDetails>> (int id) =>
+        group.MapGet("/{id}", Results<Ok<Todo>, NotFound<ProblemDetails>> ([Description("The ID of the todo to get")] int id) =>
         {
             if (todos.ContainsKey(id))
             {
                 return TypedResults.Ok(todos[id]);
             }
             return TypedResults.NotFound<ProblemDetails>(null);
-        });
+        })
+        .WithDescription("Get a specific todo by id")
+        .WithName("GetTodo");
 
         // Create or replace a new todo
-        group.MapPut("/{id}", Results<Ok<Todo>, Created<Todo>> (int id, Todo Todo) =>
+        group.MapPut("/{id}", Results<Ok<Todo>, Created<Todo>> ([Description("The ID of the todo to create")] int id, [Description("The todo item contents")] Todo Todo) =>
         {
             bool exists = todos.ContainsKey(id);
             todos[id] = Todo;
             return exists ? TypedResults.Ok(Todo) : TypedResults.Created($"/{id}", Todo);
-        });
+        })
+        .WithDescription("Create or replace a new todo")
+        .WithName("CreateTodo");
 
         // Delete a todo
-        group.MapDelete("/{id}", Results<NoContent, NotFound<ProblemDetails>> (int id) =>
+        group.MapDelete("/{id}", Results<NoContent, NotFound<ProblemDetails>> ([Description("The ID of the todo to delete")] int id) =>
         {
             if (todos.ContainsKey(id))
             {
@@ -46,7 +55,9 @@ internal static class TodosApi
                 return TypedResults.NoContent();
             }
             return TypedResults.NotFound<ProblemDetails>(null);
-        });
+        })
+        .WithDescription("Delete a todo")
+        .WithName("DeleteTodo");
 
         return group;
     }
